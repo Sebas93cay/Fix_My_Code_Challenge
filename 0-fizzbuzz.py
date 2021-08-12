3b2cb4da57cafb23588724fe48c21a34e21a8d81
Seb Asus VS <sebas93cay@gmail.com>
2021-08-12 17:12:22 -0500

    Fix from 0 to 4

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -17,14 +17,22 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
+        fb = ""
         if (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
-        elif (i % 5) == 0:
-            tmp_result.append("Buzz")
-        else:
-            tmp_result.append(str(i))
+            fb = "Fizz"
+        if (i % 5) == 0:
+            fb += "Buzz"
+        if fb == "":
+            fb = str(i)
+        tmp_result.append(fb)
+        # if (i % 3) == 0:
+        # tmp_result.append("Fizz")
+        # elif (i % 3) == 0 and (i % 5) == 0:
+        # tmp_result.append("FizzBuzz")
+        # elif (i % 5) == 0:
+        # tmp_result.append("Buzz")
+        # else:
+        # tmp_result.append(str(i))
     print(" ".join(tmp_result))
 
 
